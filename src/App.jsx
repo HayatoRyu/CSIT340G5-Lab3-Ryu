03aa7fb988e58e57ac70cbe76534fc94634968e7
HayatoRyu <hayatoryu58@gmail.com>
2026-09-21 20:05:07 +0800

    1.1 course information step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
-const App = () => {
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Content = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>
+}
+
+const App = () => {
+  const course = 'CIT-U Subjects'
+  const part1 = 'CSIT340 - Industry Elective 1'
+  const exercises1 = 6
+  const part2 = 'CSIT321 - Applications Development and Emerging Technologies'
+  const exercises2 = 6
+  const part3 = 'CSIT327 - Information Management 2'
+  const exercises3 = 6
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
