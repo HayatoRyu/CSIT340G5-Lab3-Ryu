c17b2ac99a7ada57783b6fe2d42d7a9c859e68a7
HayatoRyu <hayatoryu58@gmail.com>
2026-09-21 20:16:31 +0800

    1.4: course information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,30 @@ const Part = (props) => (
 
 const Content = (props) => (
   <div>
-    <Part part={props.part1} />
-    <Part part={props.part2} />
-    <Part part={props.part3} />
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
   </div>
 )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Number of exercises {total}</p>
+}
 
 const App = () => {
   const course = 'CIT-U Subjects'
-  const part1 = { name: 'CSIT340 - Industry Elective 1', exercises: 6 }
-  const part2 = { name: 'CSIT321 - Applications Development and Emerging Technologies', exercises: 6 }
-  const part3 = { name: 'CSIT327 - Information Management 2', exercises: 6 }
+  const parts = [
+    { name: 'CSIT340 - Industry Elective 1', exercises: 6 },
+    { name: 'CSIT321 - Applications Development and Emerging Technologies', exercises: 6 },
+    { name: 'CSIT327 - Information Management 2', exercises: 6 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
