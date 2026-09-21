51536f2a7cf8ce0c62e3ca0bde0f0ec76f6aef55
HayatoRyu <hayatoryu58@gmail.com>
2026-09-21 20:12:49 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,33 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.part1} exercises={props.exercises1} />
-      <Part name={props.part2} exercises={props.exercises2} />
-      <Part name={props.part3} exercises={props.exercises3} />
-    </div>
-  )
-}
+const Content = (props) => (
+  <div>
+    <Part part={props.part1} />
+    <Part part={props.part2} />
+    <Part part={props.part3} />
+  </div>
+)
 
-const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>
-}
+const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const App = () => {
   const course = 'CIT-U Subjects'
-  const part1 = 'CSIT340 - Industry Elective 1'
-  const exercises1 = 6
-  const part2 = 'CSIT321 - Applications Development and Emerging Technologies'
-  const exercises2 = 6
-  const part3 = 'CSIT327 - Information Management 2'
-  const exercises3 = 6
+  const part1 = { name: 'CSIT340 - Industry Elective 1', exercises: 6 }
+  const part2 = { name: 'CSIT321 - Applications Development and Emerging Technologies', exercises: 6 }
+  const part3 = { name: 'CSIT327 - Information Management 2', exercises: 6 }
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
